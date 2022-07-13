f72d519bf9710092322d460dd72888c8d2e2ef5f
ThatKid <1047800338@qq.com>
2022-07-13 14:17:00 +0800

    first commit
    Initialize the

---
+++ Carthage/Build/iOS/TestFramework.framework/Headers/test.swift
@@ -0,0 +1,14 @@
+//
+//  test.swift
+//  TestFramework
+//
+//  Created by ruihan on 2022/7/12.
+//
+
+import Foundation
+public struct test {
+    public init(){}
+    public static func getStr() -> String {
+        return "test"
+    }
+}
