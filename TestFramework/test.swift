9b51a614e62628ea7dd483f265ad351ac2666fbd
ThatKid <1047800338@qq.com>
2022-07-14 15:20:39 +0800

    Add the Alamofire dependency

--- TestFramework/test.swift
+++ TestFramework/test.swift
@@ -6,9 +6,18 @@
 //
 
 import Foundation
+import Alamofire
+
 public struct test {
+    
     public init(){}
     public static func getStr() -> String {
         return "test"
+    }
+    
+    public func testAl() {
+        AF.request("https://httpbin.org/get").response { response in
+            print(response)
+        }
     }
 }
